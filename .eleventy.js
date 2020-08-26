365ac68eb6bb5270669eec56b7990f6f26065faf
sashaqred <sashaqred@ya.ru>
2020-08-26 13:12:53 +0300

    feat: add helmet for setting tags in head

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
+const eleventyHelmetPlugin = require('eleventy-plugin-helmet');
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyHelmetPlugin);
   eleventyConfig.addPassthroughCopy({ 'src/assets': '.' });
 
   return {
