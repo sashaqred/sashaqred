eb0cb0fb417c1bb06492840f738bb6d285223ed2
sashaqred <sashaqred@ya.ru>
2020-08-26 09:45:55 +0300

    style: apply formatting rules

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ 'src/assets': '.' });
 
   return {
@@ -6,5 +6,5 @@ module.exports = function(eleventyConfig) {
       input: 'src',
       output: 'dist',
     },
-  }
+  };
 };
