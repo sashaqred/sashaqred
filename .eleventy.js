a0f770a4bdf6ce2971d339756fad092253a53bb2
sashaqred <sashaqred@ya.ru>
2020-10-30 19:56:43 +0300

    feat: add syntax highlighter

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const eleventyHelmetPlugin = require('eleventy-plugin-helmet');
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyHelmetPlugin);
+  eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPassthroughCopy({ '_eleventy/public': '.' });
   eleventyConfig.setUseGitIgnore(false);
 
