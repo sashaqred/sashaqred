79bb66ebdf565ad5ad3bb5b55542d4bbea32641d
sashaqred <sashaqred@ya.ru>
2020-11-04 23:17:10 +0300

    feat: add initial table of contents generation

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
 const eleventyHelmetPlugin = require('eleventy-plugin-helmet');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const eleventyPluginToc = require('eleventy-plugin-toc');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setLibrary('md', markdownIt({ html: true }).use(markdownItAnchor));
   eleventyConfig.addPlugin(eleventyHelmetPlugin);
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(eleventyPluginToc);
   eleventyConfig.addPassthroughCopy({ '_eleventy/public': '.' });
   eleventyConfig.setUseGitIgnore(false);
 
