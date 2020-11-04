600a54c456aa45bf8e6e6d0a1ec0abe3e8273ea7
sashaqred <sashaqred@ya.ru>
2020-11-04 22:50:44 +0300

    build: add files cleaning before serve and build

--- tasks/eleventy.js
+++ tasks/eleventy.js
@@ -1,11 +1,19 @@
 const { spawn } = require('child_process');
 const { resolve } = require('path');
 const { src, dest, watch, series, parallel } = require('gulp');
+const clean = require('gulp-clean');
 const { processCss, watchCss } = require('./style');
 
 const eleventySourcePath = resolve(__dirname, '../src/**/*');
 const eleventySourceIgnorePath = resolve(__dirname, '../src/styles/**');
 const eleventyDistPath = resolve(__dirname, '../_eleventy');
+const eleventyOutputPath = resolve(__dirname, '../dist');
+
+function cleanEleventy() {
+  return src([eleventyDistPath, eleventyOutputPath], { allowEmpty: true }).pipe(
+    clean({ force: true }),
+  );
+}
 
 function copyEleventy() {
   return src(eleventySourcePath, { ignore: eleventySourceIgnorePath }).pipe(dest(eleventyDistPath));
@@ -15,7 +23,7 @@ function watchEleventy() {
   return watch([eleventySourcePath], series(copyEleventy));
 }
 
-const buildEleventy = series(processCss, copyEleventy, () =>
+const buildEleventy = series(cleanEleventy, processCss, copyEleventy, () =>
   spawn('eleventy', [], {
     shell: true,
     stdio: 'inherit',
@@ -25,6 +33,7 @@ const buildEleventy = series(processCss, copyEleventy, () =>
 exports.eleventyBuild = buildEleventy;
 
 exports.eleventyServe = series(
+  cleanEleventy,
   buildEleventy,
   parallel(watchCss, watchEleventy, () =>
     spawn('eleventy', ['--serve'], {
