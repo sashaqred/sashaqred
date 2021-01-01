98ff24c1c91fc150142b4f4774ac56cff1364b5e
sashaqred <sashaqred@ya.ru>
2021-01-02 01:28:43 +0300

    feat: add initial i18n

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,11 @@ const eleventyHelmetPlugin = require('eleventy-plugin-helmet');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const eleventyPluginToc = require('eleventy-plugin-toc');
 const eleventyPluginReadingTime = require('eleventy-plugin-reading-time');
+const i18n = require('eleventy-plugin-i18n');
 const date = require('./src/_filters/date');
 const linkToSectionInstall = require('./src/_filters/link-to-section');
 const md = require('./src/_markdown-it');
+const translations = require('./src/i18n');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setLibrary('md', md);
@@ -12,6 +14,12 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(eleventyPluginToc);
   eleventyConfig.addPlugin(eleventyPluginReadingTime);
+  eleventyConfig.addPlugin(i18n, {
+    translations,
+    fallbackLocales: {
+      '*': 'en',
+    },
+  });
   eleventyConfig.addPassthroughCopy({ '_eleventy/public': '.' });
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addFilter('date', date);
