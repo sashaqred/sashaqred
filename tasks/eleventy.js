7cc30a6bee6bbb24744694d48ed15481bc7f1eaa
sashaqred <sashaqred@ya.ru>
2021-01-02 02:59:56 +0300

    fix: fix post deplay cname overwrite

--- tasks/eleventy.js
+++ tasks/eleventy.js
@@ -5,6 +5,7 @@ const clean = require('gulp-clean');
 const { processCss, watchCss } = require('./style');
 
 const eleventySourcePath = resolve(__dirname, '../src/**/*');
+const cnamePath = resolve(__dirname, '../CNAME');
 const eleventySourceIgnorePath = resolve(__dirname, '../src/styles/**');
 const eleventyDistPath = resolve(__dirname, '../_eleventy');
 const eleventyOutputPath = resolve(__dirname, '../dist');
@@ -19,15 +20,24 @@ function copyEleventy() {
   return src(eleventySourcePath, { ignore: eleventySourceIgnorePath }).pipe(dest(eleventyDistPath));
 }
 
+function copyCname() {
+  return src(cnamePath, { ignore: eleventySourceIgnorePath }).pipe(dest(eleventyOutputPath));
+}
+
 function watchEleventy() {
   return watch([eleventySourcePath], series(copyEleventy));
 }
 
-const buildEleventy = series(cleanEleventy, processCss, copyEleventy, () =>
-  spawn('eleventy', [], {
-    shell: true,
-    stdio: 'inherit',
-  }),
+const buildEleventy = series(
+  cleanEleventy,
+  processCss,
+  copyEleventy,
+  () =>
+    spawn('eleventy', [], {
+      shell: true,
+      stdio: 'inherit',
+    }),
+  copyCname,
 );
 
 exports.eleventyBuild = buildEleventy;
