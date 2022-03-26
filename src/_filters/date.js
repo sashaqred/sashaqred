2daa942dafab97bbcf019537cba64d7f9a305343
sashaqred <sashaqred@ya.ru>
2022-03-26 14:58:17 +0400

    feat: render full month name by default in date filter

--- src/_filters/date.js
+++ src/_filters/date.js
@@ -1,7 +1,7 @@
 const { format } = require('date-fns');
 const locales = require('date-fns/locale');
 
-module.exports = function (value, lang, dateFormat = 'd MMM u') {
+module.exports = function (value, lang, dateFormat = 'd MMMM u') {
   const locale = lang !== 'en' ? locales[lang] : undefined;
   const formatted = format(value, dateFormat, { locale });
   return formatted;
