b169e174beb3b9eeaa16c954d03b3437213165fc
sashaqred <sasha@sashaqred.com>
2025-12-22 20:06:57 +0100

    build: add css minification

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 import autoprefixer from 'autoprefixer';
 import atImport from 'postcss-import';
+import postcssMinify from '@csstools/postcss-minify';
 
 export default {
-  plugins: [atImport({ filter: (path) => path?.includes('/node_modules/') }), autoprefixer()],
+  plugins: [
+    atImport({ filter: (path) => path?.includes('/node_modules/') }),
+    autoprefixer(),
+    postcssMinify,
+  ],
 };
